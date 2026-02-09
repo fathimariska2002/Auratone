e276dec7a982fcf936fcabf8eb676f6add159f2e
fathimariska2002 <147105318+fathimariska2002@users.noreply.github.com>
2026-02-09 11:39:06 +0530

    Add files via upload
    android

---
+++ Auratone/android/app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.pin_biometric_offline"
+    // Use explicit SDK versions to support emulator API 36
+    compileSdk = 36
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.pin_biometric_offline"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        // Biometric APIs require at least API 23 on Android
+        minSdk = flutter.minSdkVersion
+        targetSdk = 36
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ Auratone/android/app/src/main/kotlin/com/example/pin_biometric_offline/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pin_biometric_offline
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
